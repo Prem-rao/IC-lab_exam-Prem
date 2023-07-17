06ff225ad178bd596bd687f2a4a4ba455638c736
Prem-rao <raopremwaghmare@gmail.com>
2023-07-17 16:31:43 +0530

    update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import "bootstrap/dist/js/bootstrap.bundle";
 import Home from "./components/Home"
 import Destination from "./components/Destination";
+import NavBar from './NavBar'
 function App() {
   return (
    <>
---
+++ myapp/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h2 className='title'>Build a responsive website for Travel sector which containing the following:</h2>
+    </div>
+  )
+}
+
+export default About
