35b9538dd1cc2d1cb90262fff4d52debaa2b41e3
Prem-rao <raopremwaghmare@gmail.com>
2023-07-17 18:47:26 +0530

    update

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,13 +3,15 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import "bootstrap/dist/js/bootstrap.bundle";
 import Home from "./components/Home"
 import Destination from "./components/Destination";
-import NavBar from './NavBar'
+import NavBar from './components/NavBar'
 function App() {
   return (
    <>
     <BrowserRouter>
+    <NavBar/>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/destination" element={<Destination/>} />
       <Route path="/destination" element={<Destination/>} />
     </Routes>
     </BrowserRouter>
