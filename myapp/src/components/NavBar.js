1565cd8e265cf9bd29f63fe364771161db0948ea
Prem-rao <raopremwaghmare@gmail.com>
2023-07-17 16:23:14 +0530

    Added components

---
+++ myapp/src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
+const NavBar = () => {
+  return (
+    <>
+        <nav class="navbar  navbar-expand bg-dark navbar-dark">
+            <div class="container-fluid">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <Link class="nav-link" to={"/"}>Home</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link class="nav-link" to={"/contact"}>Destination</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link class="nav-link" to={"/about"}>About</Link>
+                    </li>
+                </ul>
+                <Link className='btn btn-primary mx-1' to="/login">Login</Link>
+                <Link className='btn btn-primary mx-1' to="/sign-up">SignUp</Link>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default NavBar
